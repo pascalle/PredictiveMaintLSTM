3c72d59577df059fe8a380fbf00c9fae7fea3aeb
Pascalle <pb.fsu10@gmail.com>
2022-02-19 14:48:51 +0100

    updating

---
+++ PredMaintLSTM.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 19 14:23:18 2022
+
+@author: mw
+"""
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#wd = r'your_working__directory'
+wd = 'C:/Users/mw/OneDrive/Desktop/Predictive Maintenance with LSTM Project'
+os.chdir(wd)
+
+data_path = "Data/GT_FD001"
